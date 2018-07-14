c3b05830bb3247e68217d179c823d9fe2478b6e8
teramotodaiki <i101323@gmail.com>
2018-07-14 16:15:00 +0900

    feature: マップの型を定義

---
+++ src/types.ts
@@ -0,0 +1,30 @@
+export type Index = number;
+export type Table = Index[][];
+export type TileSize = [number, number];
+export type TileImageType = 'data-url';
+export type TileImage = {
+  type: TileImageType;
+  src: string;
+};
+export type TileOrder = 'Inherit' | 'Below' | 'SortY' | 'Above';
+export type TileCollider = boolean[];
+export type TileAuthor = {
+  name: string;
+  url?: string;
+};
+export type Square = {
+  index: Index;
+  tile: {
+    size: TileSize;
+    image: TileImage;
+    order: TileOrder;
+    collider: TileCollider;
+    author: TileAuthor;
+  };
+};
+export type Scene = {
+  map: {
+    tables: Table[];
+    squares: Square[];
+  };
+};
