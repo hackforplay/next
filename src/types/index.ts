e3e86181469d8b4b9047665ce47ceffc552a5660
teramotodaiki <i101323@gmail.com>
2018-07-23 16:13:52 +0900

    feature: タイルサイズのタイプを可変配列に変更

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,7 +5,7 @@ export type Placement = Placement.Placement;
 
 export type Index = number;
 export type Table = Index[][];
-export type TileSize = [number, number];
+export type TileSize = number[];
 export type TileImageType = 'data-url';
 export type TileImage = {
   type: TileImageType;
