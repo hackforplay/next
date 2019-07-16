4fda44aeccb8128426d896a1cf2eb99e984a073d
teramotodaiki <i101323@gmail.com>
2019-07-16 14:39:02 +0900

    fix: data-url でなくとも src を読み込む

--- src/preloader/load-images.ts
+++ src/preloader/load-images.ts
@@ -11,26 +11,23 @@ export default async function loadImages(scene: t.Scene) {
 
   for (const square of scene.map.squares) {
     if (existIndexes.indexOf(square.index) > -1) continue;
-    const { type, src } = square.tile.image;
-    if (type === 'data-url') {
-      loadingAssets.push(
-        new Promise((resolve, reject) => {
-          const image = new Image();
-          const asset = {
-            image,
-            index: square.index,
-            isLoading: true
-          };
-          image.onload = () => {
-            asset.isLoading = false;
-            resolve();
-          };
-          image.onerror = e => reject(e);
-          image.src = src;
-          scene.assets.images.push(asset);
-        })
-      );
-    }
+    loadingAssets.push(
+      new Promise((resolve, reject) => {
+        const image = new Image();
+        const asset = {
+          image,
+          index: square.index,
+          isLoading: true
+        };
+        image.onload = () => {
+          asset.isLoading = false;
+          resolve();
+        };
+        image.onerror = e => reject(e);
+        image.src = square.tile.image.src;
+        scene.assets.images.push(asset);
+      })
+    );
   }
 
   await Promise.all(loadingAssets);
